52029c7cbf6498f20962d6506f7db3f146325e27
Shashank Salunkhe <shashank.salunkhe@itinnovate.com.au>
2021-09-24 23:58:44 +1000

    refactor: restructure App js and related files

---
+++ ui/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Custom components import
+import MainBody from "../MainBody";
+
+import "../../App.css";
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<MainBody />
+		</React.Fragment>
+	);
+};
+
+export default App;
